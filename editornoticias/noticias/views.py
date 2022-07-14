8aeaf28c1af2c6f2ea6b50862c67806b9d48083d
juanpablosalas <60354412+juanpablosalas@users.noreply.github.com>
2022-07-14 17:11:46 -0500

    Archivos static y URL para las noticias

--- editornoticias/noticias/views.py
+++ editornoticias/noticias/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from . models import Noticia
 
 class VistaNoticias(ListView):
     model = Noticia
     template_name = 'home.html'
+    
+class VistaNoticiaDetallada(DetailView):
+    model = Noticia
+    template_name = 'detail.html'
 
 # Create your views here.
