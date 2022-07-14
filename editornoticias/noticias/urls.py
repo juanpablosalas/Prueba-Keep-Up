9c654cc071cab16af7c8341cdad2fb6628a9afd7
juanpablosalas <60354412+juanpablosalas@users.noreply.github.com>
2022-07-14 16:29:49 -0500

    Config administrador y URLs

---
+++ editornoticias/noticias/urls.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 14 16:26:32 2022
+
+@author: juanpablosalas
+"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.VistaNoticias.as_view(), name='home'),
+]
--- editornoticias/noticias/views.py
+++ editornoticias/noticias/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from . models import Noticia
+
+class VistaNoticias(ListView):
+    model = Noticia
+    template_name = 'home.html'
 
 # Create your views here.
