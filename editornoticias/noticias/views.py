bcdd3500782ce76230d3a60e2e2393e899adcb47
juanpablosalas <60354412+juanpablosalas@users.noreply.github.com>
2022-07-18 22:42:27 -0500

    Notificaciones para el usuario implementadas
    Recien añadida, recién eliminada y recién actualizada una noticia

--- editornoticias/noticias/views.py
+++ editornoticias/noticias/views.py
@@ -19,7 +19,7 @@ class VistaNoticiaDetallada(DetailView):
 def home(request):
     noticias = Noticia.objects.all()
 
-    return render(request,'home.html',{'object_list':noticias})
+    return render(request,'home.html',{'object_list':noticias, 'justadded': False, 'justdeleted': False})
 
 def detail(request, pk):
     print("-- La recibe")
@@ -33,7 +33,7 @@ def detail(request, pk):
         elif request.POST.get("confirm-delete"):
             delete_warning = True
             Noticia.objects.filter(id=pk).delete()
-            return render(request,'home.html',{'object_list':Noticia.objects.all()})  
+            return render(request,'home.html',{'object_list':Noticia.objects.all(), 'justadded': False, 'justdeleted': True})  
         elif request.POST.get("cancel-delete"):
             delete_warning = False
                 
@@ -43,6 +43,8 @@ def detail(request, pk):
 
 def add(request):
     
+    error_warning = False
+    
     if request.method == 'POST':
         
         titulo = request.POST['titulo']
@@ -50,15 +52,22 @@ def add(request):
         fecha = datetime.now()
         cuerpo = request.POST['cuerpo']
         
-        noticia = Noticia.objects.create(
-            autor = autor, 
-            titulo = titulo, 
-            fecha=fecha, 
-            cuerpo=cuerpo)
         
-        return render(request,'home.html',{'object_list':Noticia.objects.all()})
         
-    return render(request, 'add.html')
+        if(titulo=="" or autor =="" or cuerpo==""):
+                error_warning = True
+        else:
+            
+            Noticia.objects.create(
+                autor = autor, 
+                titulo = titulo, 
+                fecha=fecha, cuerpo=cuerpo)
+            return render(request,'home.html',{'object_list':Noticia.objects.all(), 'justadded': True, 'justdeleted': False})
+        
+        
+        
+        
+    return render(request, 'add.html', {'warning': error_warning})
 
 def edit(request, pk):
     noti = Noticia.objects.get(id=pk)
